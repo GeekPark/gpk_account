7256ff6634f192eccc0c0266c76c4a20d3dfb166
cloudy9101 H <cloudy9101@gmail.com>
2016-03-10 15:39:11 +0800

    Add user and authorization basic tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { build(:user) }
+
+  describe '.save' do
+    context 'with valid email,mobile,username' do
+      it { expect(user).to be_valid }
+    end
+
+    context 'without email,mobile' do
+      it do
+        user.email = user.mobile = ''
+        expect(user).not_to be_valid
+      end
+    end
+
+    context 'with only email' do
+      it do
+        user.mobile = user.username = nil
+        expect(user).to be_valid
+      end
+    end
+
+    context 'with only mobile' do
+      it do
+        user.email = user.username = nil
+        expect(user).to be_valid
+      end
+    end
+  end
 end
