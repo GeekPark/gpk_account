a5ab2469010b25e1e78dc5a1cff8d9071eef3f17
cloudy9101 H <cloudy9101@gmail.com>
2016-03-14 16:26:30 +0800

    Add user and authorization basic validates

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -1,2 +1,7 @@
 class Authorization < ActiveRecord::Base
+  belongs_to :user
+
+  validates :platform, presence: true
+  validates :open_id, presence: { if: proc { platform == 'wechat' } }
+  validates :uid, presence: { if: proc { platform == 'weibo' } }
 end
