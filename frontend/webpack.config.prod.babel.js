eddefbe94e6ccd8638890be25d5c169855ba1031
Vanilla X <mail@liyaodong.com>
2016-04-22 12:55:14 +0800

    Add webpack bundle command

--- frontend/webpack.config.prod.babel.js
+++ frontend/webpack.config.prod.babel.js
@@ -1,15 +1,21 @@
-const path = require('path');
-const config = require('./webpack.config');
-const ManifestPlugin = require('webpack-manifest-plugin');
+import { optimize } from 'webpack';
+import path from 'path';
+import config from './webpack.config.babel';
+import ManifestPlugin from 'webpack-manifest-plugin';
 
 config.output.filename = '[name]_bundle-[chunkhash].js';
 config.output.publicPath = 'http://7xjjng.com2.z0.glb.qiniucdn.com/static/';
 config.output.path = path.join(__dirname, '../', 'public', 'static');
 
-config.plugins = config.plugins.concat([
+config.plugins = [...config.plugins, (
   new ManifestPlugin({
     fileName: 'webpack_manifest.json',
-  }),
-]);
+  })),
+  (new optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true,
+    },
+  })),
+];
 
 module.exports = config;
