c53ad3c244855600aa24845d2846227cfb197ae0
Vanilla X <mail@liyaodong.com>
2016-04-25 11:03:55 +0800

    Add validatorimg modal component

--- frontend/__tests__/reducers/modal-test.js
+++ frontend/__tests__/reducers/modal-test.js
@@ -8,6 +8,7 @@ describe('modal reducer', () => {
   const before = {
     isOpen: false,
     modalName: null,
+    modalStyle: {},
   };
 
   deepFreeze(before);
@@ -16,16 +17,37 @@ describe('modal reducer', () => {
     const after = {
       isOpen: true,
       modalName: 2,
+      modalStyle: {
+        overlay: {
+          background: '#000',
+        },
+      },
     };
 
     expect(
-      modal(before, { type: 'OPEN_MODAL', modalName: 2 })
+      modal(before, { type: 'OPEN_MODAL', modalName: 2, modalStyle: {
+        overlay: {
+          background: '#000',
+        },
+      } })
+    ).toEqual(after);
+  });
+
+  it('open modal with undefined style', () => {
+    const after = {
+      isOpen: true,
+      modalName: 3,
+      modalStyle: {},
+    };
+
+    expect(
+      modal(before, { type: 'OPEN_MODAL', modalName: 3 })
     ).toEqual(after);
   });
 
   it('close modal', () => {
     expect(modal(before, { type: 'CLOSE_MODAL' })).toEqual({
-      isOpen: false, modalName: null,
+      isOpen: false, modalName: null, modalStyle: {},
     });
   });
 });
