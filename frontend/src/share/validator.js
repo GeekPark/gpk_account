e0c066e8c8b32d25f9c3ec3f09892e3bce5eaad9
Vanilla X <mail@liyaodong.com>
2016-04-25 11:29:34 +0800

    Add email validator

--- frontend/src/share/validator.js
+++ frontend/src/share/validator.js
@@ -5,3 +5,8 @@ export function isPhoneNumber(str) {
 export function isNotEmpty(str) {
   return (typeof str !== 'undefined') && str.length > 0;
 }
+
+export function isEmail(str) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(str);
+}
