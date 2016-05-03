e7a4415e26389de9935dabe08bd9d98b8359d8b6
Vanilla X <mail@liyaodong.com>
2016-05-03 11:18:20 +0800

    Add left menu UI

--- frontend/src/components/users/LeftMenu.jsx
+++ frontend/src/components/users/LeftMenu.jsx
@@ -8,13 +8,18 @@ class LeftMenu extends React.Component {
     const { nickname } = server.user;
     return (
       <div className="left-menu">
-        <section className="section basic-userinfo">
+        <section className="section basic-userinfo mb-bottom">
           <div className="avatar-wrapper">
             <Avatar />
           </div>
           <div className="nickname">
             {nickname}
           </div>
+        </section>
+        <section className="section menu-items no-padding">
+          <a href="javascript:;">基本资料</a>
+          <a className="active" href="javascript:;">账户安全</a>
+          <a href="javascript:;">第三方账号</a>
         </section>
       </div>
     );
