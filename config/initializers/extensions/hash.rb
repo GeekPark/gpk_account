1447eb6c4e510e372e5fe4d557df0e8291e194e9
Gavin Li <gavin.li1986@gmail.com>
2016-05-12 17:14:40 +0800

    Fix register api bug && Add tests

--- config/initializers/extensions/hash.rb
+++ config/initializers/extensions/hash.rb
@@ -2,9 +2,9 @@ class Hash
   def flatten_nested
     each_with_object({}) do |(k, v), h|
       if v.is_a? Hash
-        v.flatten_nested.map { |h_k, h_v| h["#{k}.#{h_k}"] = h_v }
+        v.flatten_nested.map { |h_k, h_v| h["#{k}.#{h_k}"] = h_v.to_s }
       else
-        h[k] = v
+        h[k.to_s] = v.to_s
       end
     end
   end
