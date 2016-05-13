172227275d0308c87b54415883d59553582e4a73
Vanilla X <mail@liyaodong.com>
2016-05-13 14:29:02 +0800

    Complete third accound bind && unbind

--- frontend/src/components/users/Third.jsx
+++ frontend/src/components/users/Third.jsx
@@ -2,31 +2,38 @@ import React, { PropTypes } from 'react';
 
 import Main from './Main';
 
+const Item = ({ type, isBind }) => (
+  <div className="form-button-group">
+    <div className="left-label">
+      <span className="label-text">
+        <i className={`iconfont icon-${type} ${isBind ? 'active' : ''}`}></i>
+        微博
+      </span>
+      <span className="provider-name">
+        { isBind ? '已绑定' : '未绑定' }
+      </span>
+    </div>
+    <a href={`/auth/${type}${isBind ? '/unbind' : ''}`} data-method="DELETE" className="form-button">
+      { isBind ? '取消绑定' : '立即绑定' }
+    </a>
+  </div>
+);
+
+Item.propTypes = {
+  type: PropTypes.string.isRequired,
+  isBind: PropTypes.bool.isRequired,
+};
+
 class Third extends React.Component {
   render() {
+    const { authorizations } = this.props.server.user;
+    const weibo = authorizations.filter(x => x.provider === 'weibo')[0];
+    const wechat = authorizations.filter(x => x.provider === 'wechat')[0];
     return (
       <Main title="第三方帐号" desc="可直接使用绑定的第三方帐号登录你的帐号">
         <div className="third-list">
-          <div className="form-button-group">
-            <div className="left-label">
-              <span className="label-text">
-                <i className="iconfont icon-wechat"></i>
-                微信
-              </span>
-              <span className="provider-name">未绑定</span>
-            </div>
-            <a href="/auth/wechat" className="form-button">立即绑定</a>
-          </div>
-          <div className="form-button-group">
-            <div className="left-label">
-              <span className="label-text">
-                <i className="iconfont icon-weibo"></i>
-                微博
-              </span>
-              <span className="provider-name">未绑定</span>
-            </div>
-            <a href="javascript:;" className="form-button">立即绑定</a>
-          </div>
+          <Item type="wechat" isBind={wechat !== undefined} />
+          <Item type="weibo" isBind={weibo !== undefined} />
         </div>
       </Main>
     );
