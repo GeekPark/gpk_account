7f19fdbba2d44c41bed5af0ddb34c4613589151f
Gavin Li <gavin.li1986@gmail.com>
2016-05-17 18:10:48 +0800

    Change back user api position attribute

--- app/serializers/user_basic_serializer.rb
+++ app/serializers/user_basic_serializer.rb
@@ -1,16 +1,9 @@
 class UserBasicSerializer < ActiveModel::Serializer
-  attributes :id, :nickname, :realname, :gender, :birthday, :city, :bio, :position, :avatar_url
+  attributes :id, :nickname, :city, :realname, :gender, :birthday, :company, :title, :bio, :avatar_url
 
   def city
     ChinaCity.get(object.city.to_s, prepend_parent: true)
   rescue NoMethodError
     ''
-  end
-
-  def position
-    {
-      company: object.company,
-      title: object.title
-    }
   end
 end
