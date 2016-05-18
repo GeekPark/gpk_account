caccfd5d12fc664d2cf63c63034881b6957d75ea
Gavin Li <gavin.li1986@gmail.com>
2016-05-18 10:35:21 +0800

    Hide mobile and email information

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,13 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :nickname, :mobile, :avatar_url, :realname, :gender,
     :birthday, :city, :company, :title, :bio, :is_old
-
   has_many :authorizations
+
+  def mobile
+    object.mobile.gsub(/(?<=\d{3})\d(?=\d{4})/, '*')
+  end
+
+  def email
+    object.email.gsub(/(?!\A).+(?=@)/, '****')
+  end
 end
