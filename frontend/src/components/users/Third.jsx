b5a16965d4977fef7384e9d1b1e29889a9e958c3
Vanilla X <mail@liyaodong.com>
2016-05-19 13:33:19 +0800

    Complete sns user && old user ban rule

--- frontend/src/components/users/Third.jsx
+++ frontend/src/components/users/Third.jsx
@@ -3,16 +3,19 @@ import React, { PropTypes } from 'react';
 import Main from './Main';
 import Item from './ThirdItem';
 
+import { isSNS } from '../../share/utils';
+
 class Third extends React.Component {
   render() {
     const { authorizations } = this.props.server.user;
     const weibo = authorizations.filter(x => x.provider === 'weibo')[0];
     const wechat = authorizations.filter(x => x.provider === 'wechat')[0];
+    const snsUser = isSNS(this.props.server.user);
     return (
       <Main title="第三方帐号" desc="可直接使用绑定的第三方帐号登录你的帐号">
         <div className="third-list">
-          <Item type="wechat" isBind={wechat !== undefined} dispatch={this.props.dispatch} />
-          <Item type="weibo" isBind={weibo !== undefined} dispatch={this.props.dispatch} />
+          <Item type="wechat" isBind={wechat !== undefined} isSNS={snsUser} dispatch={this.props.dispatch} />
+          <Item type="weibo" isBind={weibo !== undefined} isSNS={snsUser} dispatch={this.props.dispatch} />
         </div>
       </Main>
     );
