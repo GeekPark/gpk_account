678658b9746c18fa6cfb47a663f7fbb1d82a4f44
Vanilla X <mail@liyaodong.com>
2016-08-23 18:24:33 +0800

    Automic leftmenu && turbolinks support

--- frontend/src/admin.js
+++ frontend/src/admin.js
@@ -1,19 +1,35 @@
 /* eslint import/no-unresolved: 0 */
 
 import $ from 'jquery';
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { render } from './share/utils';
 
-import LeftMenu from './components/admin/LeftMenu';
+import Layout from './components/admin/Layout';
 
 // apply antd style
 require('style!css!antd/dist/antd.min.css');
 
 class App extends React.Component {
+  static propTypes = {
+    server: PropTypes.object.isRequired,
+  }
+
+  static childContextTypes = {
+    server: PropTypes.object.isRequired,
+  }
+
+  getChildContext() {
+    return { server: this.props.server };
+  }
+
   render() {
     return (
       <div className="app">
-        <LeftMenu />
+        <Layout server={this.props.server}>
+          <div>
+            首页
+          </div>
+        </Layout>
       </div>
     );
   }
--- frontend/src/components/admin/Layout.jsx
+++ frontend/src/components/admin/Layout.jsx
@@ -1,13 +1,49 @@
 import React, { PropTypes } from 'react';
+import { Menu, Icon } from 'antd';
+
+const { SubMenu } = Menu;
+
+const navItems = list => list.map(x => <Menu.Item key={x.path}>
+  <a href={x.path}>{x.title}</a>
+</Menu.Item>);
 
 class Layout extends React.Component {
   static propTypes = {
-    children: PropTypes.element,
+    children: PropTypes.element.isRequired,
+  }
+
+  static contextTypes = {
+    server: PropTypes.object.isRequired,
   }
 
   render() {
+    const { action, controller, routes } = this.context.server;
+    const actionPath = action === 'index' ? '' : `/${action}`;
+
     return (
-      <div className="layout-wrapper">
+      <div className="ant-layout-aside">
+        <aside className="ant-layout-sider">
+          <a className="ant-layout-logo" href="/admin/"></a>
+          <Menu
+            mode="inline" theme="dark"
+            defaultSelectedKeys={[`/admin/${controller}${actionPath}`]}
+            defaultOpenKeys={[controller]}
+          >
+            <SubMenu key="broadcasts" title={<span><Icon type="notification" />通知中心</span>}>
+              {navItems(routes.broadcasts)}
+            </SubMenu>
+          </Menu>
+        </aside>
+        <div className="ant-layout-main">
+          <div className="ant-layout-container">
+            <div className="ant-layout-content">
+              {this.props.children}
+            </div>
+          </div>
+          <div className="ant-layout-footer">
+            GeekPark &copy; 用户中心
+          </div>
+        </div>
       </div>
     );
   }
