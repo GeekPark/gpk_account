101b2dd42d2ad204583b78c34e79fc35a1d451af
Vanilla X <mail@liyaodong.com>
2016-08-29 15:46:58 +0800

    Fix broadtype display

--- frontend/src/components/admin/broadcasts/Index.jsx
+++ frontend/src/components/admin/broadcasts/Index.jsx
@@ -1,10 +1,10 @@
 import moment from 'moment';
 import React, { PropTypes } from 'react';
-import { Table } from 'antd';
+import { Table, Icon } from 'antd';
 import { fetchBroadcasts } from '../../../share/server';
 
 const columns = [
-  { title: '类型', dataIndex: 'content_type', key: 'content_type' },
+  { title: '关联内容', dataIndex: 'content_type', key: 'content_type' },
   { title: '内容', dataIndex: 'content', key: 'content' },
   { title: '创建时间', dataIndex: 'created_at', key: 'created_at' },
   { title: '发送时间', dataIndex: 'send_at', key: 'send_at' },
@@ -18,14 +18,24 @@ const i18n = str => ({
   activity_type: '活动',
 })[str];
 
+const color = str => ({
+  topic_type: '#FF6100',
+  activity_type: '#01BAD2',
+})[str];
+
 const originLink = (type, id) => ({
   topic_type: `http://www.geekpark.net/topics/${id}`,
   activity_type: `http://events.geekpark.net/activities/${id}`,
 })[type];
 
 const transData = d => d.map(x => {
-  const action = <a href="javascript:;">删除</a>;
-  const content_type = <a href={originLink(x.content_type, x.redirect)} target="_blank">{i18n(x.content_type)}</a>;
+  const action = new Date(x.send_at).getTime() < Date.now() ? '已发送' : <a href="javascript:;">删除</a>;
+  const content_type = (
+    <a href={originLink(x.content_type, x.redirect)} target="_blank" style={{ color: color(x.content_type) }}>
+      {i18n(x.content_type)}
+      <Icon type="link" style={{ marginLeft: '5px' }} />
+    </a>
+  );
 
   return {
     ...x,
@@ -46,7 +56,7 @@ class BroadcastsIndex extends React.Component {
 
     this.state = {
       pagination: { total: context.server.broadcasts_count },
-      data: transData(context.server.broadcasts),
+      data: context.server.broadcasts,
     };
   }
 
