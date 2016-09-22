496405fa513b41fc93869ac0237e82a1eb4b4438
Shou Ya <github@lain.li>
2016-09-22 12:44:00 +0800

    Enhancing coding style

--- app/jobs/broadcast_job.rb
+++ app/jobs/broadcast_job.rb
@@ -1,47 +1,24 @@
 class BroadcastJob
   include SuckerPunch::Job
 
+  CERTIFICATE = File.read(File.join(Rails.root, ENV['CERITFICATE'])).freeze
+  PASSPHRASE = ENV['CERITFICATE_PASSWORD'].freeze
+
   def perform(broadcast_id)
-    @connection = generate_connection
     ActiveRecord::Base.connection_pool.with_connection do
       @broadcast = Broadcast.find(broadcast_id)
-      @devices = Device.all.to_a
+      @broadcast.send_to_devices(connection)
     end
-    pushing
   ensure
-    @connection&.close
+    connection.close
   end
 
   private
 
-  def pushing
-    push_broadcast @devices.shift while @devices.present?
-  rescue Errno::EPIPE
-    pushing
-  end
-
-  def push_broadcast(device)
-    message = setup_message device, @broadcast
-    @connection.write message.message
-    logger "Error: #{message.error}." if message.error
-  end
-
-  def generate_connection
-    certificate = File.read(File.join(Rails.root, ENV['CERITFICATE']))
-    passphrase = ENV['CERITFICATE_PASSWORD']
-    connection = Houston::Connection.new(APNGETEWAY, certificate, passphrase)
-    connection.open
-    connection
-  end
-
-  def setup_message(device, broadcast)
-    BroadcastsDevicesRelation.create(device_id: device.id, broadcast_id: broadcast.id)
-    message = Houston::Notification.new(device: device.id)
-    message.alert = broadcast.content
-    message.badge = device.unread_message_count
-    message.sound = 'sosumi.aiff'
-    message.content_available = false
-    message.custom_data = { data: broadcast.as_json, type: 'broadcast' }
-    message
+  def connection
+    return @connection if @connection && @connection.open?
+    @connection = Houston::Connection.new(APNGETEWAY, CERTIFICATE, PASSPHRASE)
+    @connection.open
+    @connection
   end
 end
--- app/models/broadcast.rb
+++ app/models/broadcast.rb
@@ -16,4 +16,34 @@ class Broadcast < ActiveRecord::Base
       BroadcastJob.perform_in(3, id)
     end
   end
+
+  def send_to_devices(connection)
+    devices = Device.all.to_a
+
+    begin
+      send_message(devices.shift, connection) while devices.present?
+    rescue Errno::EPIPE
+      sleep 3
+      retry
+    end
+  end
+
+  private
+
+  def send_message(device, connection)
+    BroadcastsDevicesRelation.create(device_id: device.id, broadcast_id: id)
+    notification = to_notificaiton
+    logger "Error: #{notificaiton.error}." if notifcation.error
+    connection.write notification.message
+  end
+
+  def to_notificaiton
+    Houston::Notification.new(device: device.id).tap do |msg|
+      msg.alert = broadcast.content
+      msg.badge = device.unread_message_count
+      msg.sound = 'sosumi.aiff'
+      msg.content_available = false
+      msg.custom_data = { data: broadcast.as_json, type: 'broadcast' }
+    end
+  end
 end
