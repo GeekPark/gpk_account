cddbd50197157111ec149892d42c9510ef4fc0b5
Z Kun <zhukun6150909@outlook.com>
2016-09-27 11:38:59 +0800

    Make test pass

--- config/initializers/gems/houston.rb
+++ config/initializers/gems/houston.rb
@@ -1,11 +1,12 @@
-APN = Houston::Client.public_send(Rails.env)
 filename = File.join(Rails.root, ENV['CERITFICATE'])
 
-APNGETEWAY = if Rails.env.production?
-               Houston::APPLE_PRODUCTION_GATEWAY_URI
-             else
-               Houston::APPLE_DEVELOPMENT_GATEWAY_URI
-             end
+if Rails.env.production?
+  APNGETEWAY = Houston::APPLE_PRODUCTION_GATEWAY_URI
+  APN = Houston::Client.production
+else
+  APNGETEWAY = Houston::APPLE_DEVELOPMENT_GATEWAY_URI
+  APN = Houston::Client.development
+end
 CERTIFICATE = File.read(filename).freeze if File.exist?(filename)
 PASSPHRASE = (ENV['CERITFICATE_PASSWORD'] || '').freeze
 
