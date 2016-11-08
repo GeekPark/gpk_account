dfc754fcd92b873bd5175c38906db6f131b952cf
Shou Ya <shouya@users.noreply.github.com>
2016-11-08 11:25:15 +0800

    Trying to fix #179 (#182)

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,12 @@
+# coding: utf-8
 class UserMailer < ApplicationMailer
   def send_verify_code(email, code)
     @code = code
     mail(to: email, subject: '[极客公园] Hi, 极客 您的邮箱校验码')
+  rescue Net::ReadTimeout, Net::OpenTimeout => e
+    Rails.logger.error 'Send verification code timeout'
+    Rails.logger.error e.message
+    Rails.logger.error caller
+    retry
   end
 end
