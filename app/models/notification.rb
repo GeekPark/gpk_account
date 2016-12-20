4cd40d59596e63925c91e95509c1d74bf7174dbb
啊 坤 <zhukun6150909@outlook.com>
2016-12-20 23:16:53 +0800

    Add post comment type to notification (#196)

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,12 +2,12 @@ class Notification < ActiveRecord::Base
   belongs_to :user
   belongs_to :from_user, class_name: 'User'
   validates :user_id, presence: true
-  validates :content_type, inclusion: { in: %w(event dm comment) }
+  validates :content_type, inclusion: { in: %w(event dm comment timeline_comment) }
   validates :content, presence: true
   delegate :unread_notifications_count, to: :user
   after_create :push_notification
 
-  enum content_type: { event: 1, dm: 2, comment: 3 }
+  enum content_type: { event: 1, dm: 2, comment: 3, timeline_comment: 4 }
   counter_culture :user, column_name: proc { |model| model.unread? ? 'unread_notifications_count' : nil }
 
   default_scope { order(created_at: :desc) }
