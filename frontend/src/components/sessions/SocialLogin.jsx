66652e0d3162c78d9f3909dfd67f1decae7e463b
Cedcn <cedcn@qq.com>
2017-02-07 15:31:34 +0800

    remove wechat login out of wechat

--- frontend/src/components/sessions/SocialLogin.jsx
+++ frontend/src/components/sessions/SocialLogin.jsx
@@ -1,5 +1,5 @@
 import React, { PropTypes } from 'react';
-import { isWechat } from 'mdetect';
+import { isWechat, isMobileUA } from 'mdetect';
 import { openModal } from '../../actions';
 
 class SocialLogin extends React.Component {
@@ -16,6 +16,11 @@ class SocialLogin extends React.Component {
   }
 
   render() {
+    const wechatContent = isWechat() || !isMobileUA() ? <a className="social-button-style wechat" href="javascript:;" onClick={this.wechat}>
+      <i className="iconfont icon-wechat social-icon"></i>
+      <span>微信登录</span>
+    </a> : '';
+
     return (
       <div className="social-login">
         <div className="section-title">
@@ -23,11 +28,8 @@ class SocialLogin extends React.Component {
             通过第三方帐号登录
           </span>
         </div>
-        <div className="social-button">
-          <a className="social-button-style wechat" href="javascript:;" onClick={this.wechat}>
-            <i className="iconfont icon-wechat social-icon"></i>
-            <span>微信登录</span>
-          </a>
+        <div className={`social-button ${isWechat() || !isMobileUA() ? '' : 'center'}`}>
+          {wechatContent}
           <a className="social-button-style weibo" href="/auth/weibo">
             <i className="iconfont icon-weibo social-icon"></i>
             <span>微博登录</span>
