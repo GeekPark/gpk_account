5d796e49ce9231458d4d89222523f9a870418bb6
bomb <476964702@qq.com>
2017-12-04 15:58:07 +0800

    add some info

--- app/serializers/user_basic_serializer.rb
+++ app/serializers/user_basic_serializer.rb
@@ -1,5 +1,5 @@
 class UserBasicSerializer < ActiveModel::Serializer
-  attributes :id, :nickname, :city, :realname, :gender, :birthday, :company, :title, :bio, :avatar_url, :banned, :created_days
+  attributes :id, :nickname, :city, :realname, :gender, :birthday, :company, :title, :bio, :avatar_url, :banned, :created_days, :mobile, :email
 
   def city
     ChinaCity.get(object.city.to_s, prepend_parent: true)
