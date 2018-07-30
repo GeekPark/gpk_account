c48671a5dc4b7359c297110dc497b5f073ef8cbd
Xinying <corker@126.com>
2018-07-30 10:31:51 +0800

    change cdn url

--- frontend/webpack.config.prod.babel.js
+++ frontend/webpack.config.prod.babel.js
@@ -4,7 +4,7 @@ import config from './webpack.config.babel';
 import ManifestPlugin from 'webpack-manifest-plugin';
 
 config.output.filename = '[name]_bundle-[chunkhash].js';
-config.output.publicPath = 'https://o7h2xtq3g.qnssl.com';
+config.output.publicPath = 'http://account-img.geekpark.net';
 config.output.path = path.join(__dirname, '../', 'public', 'static');
 
 config.plugins = [...config.plugins, (
